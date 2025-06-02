051f9bea594815c45cdec612d3c196ef18ca7476
Andrey-0367 <shestakov0367@yandex.ru>
2025-06-02 14:38:40 +0300

    team2

--- geology/settings.py
+++ geology/settings.py
@@ -12,20 +12,18 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='the-best-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS = ['83.166.245.78', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://83.166.245.78', 'https://83.166.245.78']
+ALLOWED_HOSTS = ['192.168.0.140', 'localhost', '127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['http://83.166.245.78', 'https://83.166.245.78']
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CORS_ALLOWED_ORIGINS = [
-    "https://www.geologiya-ru.ru",
-    "http://www.geologiya-ru.ru",
+CSRF_TRUSTED_ORIGINS = [
+    'http://192.168.0.140',
+    'https://192.168.0.140',
 ]
 
 # Дополнительные настройки для безопасности
 CORS_ALLOW_CREDENTIALS = True
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://www.geologiya-ru.ru",
-# ]
+
 
 # Application definition
 
@@ -136,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
