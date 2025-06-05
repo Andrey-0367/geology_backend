9456a771948b15719735e38467ac38983ed48e4d
Andrey-0367 <shestakov0367@yandex.ru>
2025-06-05 22:49:13 +0300

    add3

--- geology/settings.py
+++ geology/settings.py
@@ -6,9 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='the-best-secret-key')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = ['83.166.245.78', 'www.geologiya-ru.ru', 'geologiya-ru.ru']
+
+DEBUG = False
+
 
 # Application definition
 INSTALLED_APPS = [
