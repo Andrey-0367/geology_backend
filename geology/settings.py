dc7d5c6a8595c256100e27bc583052eddd466bef
Andrey-0367 <shestakov0367@yandex.ru>
2025-07-09 08:45:43 +0300

    settings

--- geology/settings.py
+++ geology/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
