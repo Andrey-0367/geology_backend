49240b5d70d051406bea725eccb42c40e70e725f
Andrey-0367 <shestakov0367@yandex.ru>
2025-07-10 11:06:34 +0300

    url-orders

--- api/urls.py
+++ api/urls.py
@@ -12,30 +12,24 @@ from .views import (
     CategoryFiltersView,
 )
 
-# Создаем основной роутер для API v1
-v1_router = routers.DefaultRouter()
-v1_router.register(r'contact', ContactMessageViewSet, basename='contact-messages')
-v1_router.register(r'employees', EmployeeViewSet, basename='employees')
-v1_router.register(r'categories', CategoryViewSet, basename='categories')
-v1_router.register(r'products', ProductViewSet, basename='products')
-v1_router.register(r'product-images', ProductImageViewSet, basename='product-images')
-v1_router.register(r'sale-items', SaleItemViewSet, basename='sale-items')
-v1_router.register(r'sale-item-images', SaleItemImageViewSet, basename='sale-item-images')
-v1_router.register(r'orders', OrderViewSet, basename='orders')
+v1_router_api = routers.DefaultRouter()
+v1_router_api.register(r'contact', ContactMessageViewSet, basename='contact-messages')
+v1_router_api.register(r'employees', EmployeeViewSet, basename='employees')
+v1_router_api.register(r'categories', CategoryViewSet, basename='categories')
+v1_router_api.register(r'products', ProductViewSet, basename='products')
+v1_router_api.register(r'product-images', ProductImageViewSet, basename='product-images')
+v1_router_api.register(r'sale-items', SaleItemViewSet, basename='sale-items')
+v1_router_api.register(r'sale-item-images', SaleItemImageViewSet, basename='sale-item-images')
+v1_router_api.register(r'orders', OrderViewSet, basename='orders')
 
-# Кастомные пути для API v1
-v1_custom_urls = [
+api_urls = [
     path('categories/<int:category_id>/filters/', CategoryFiltersView.as_view(), name='category-filters'),
     path('products/filters/', ProductViewSet.as_view({'get': 'filters'}), name='product-filters'),
 ]
 
-# Объединяем все пути API v1
-v1_urlpatterns = [
-    path('', include(v1_router.urls)),
-    *v1_custom_urls,
-]
 
-# Основные URL patterns
+api_urls.extend(v1_router_api.urls)
+
 urlpatterns = [
-    path('v1/', include(v1_urlpatterns)),
+    path('v1/', include(api_urls)),
 ]
